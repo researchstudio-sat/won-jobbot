bc2a0bee1c0b7b43e8cdb04a30b8297a7d78aafa
quasarchimaere <sudafabian@gmail.com>
2019-09-19 12:03:06 +0200

    Removes boilerplate code in favor of utils method

--- src/main/java/won/bot/jobbot/JobBotApp.java
+++ src/main/java/won/bot/jobbot/JobBotApp.java
@@ -1,30 +1,11 @@
 package won.bot.jobbot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-
-import javax.crypto.Cipher;
-import java.lang.invoke.MethodHandles;
+import won.bot.framework.bot.utils.BotUtils;
 
 public class JobBotApp {
-    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     public static void main(String[] args) throws Exception {
-        boolean failed = false;
-        if(Cipher.getMaxAllowedKeyLength("AES") != Integer.MAX_VALUE) {
-            logger.error("JCE unlimited strength encryption policy is not enabled, WoN applications will not work. Please consult the setup guide.");
-            failed = true;
-        }
-        if(System.getProperty("WON_NODE_URI") == null && System.getenv("WON_NODE_URI") == null) {
-            logger.error("WON_NODE_URI needs to be set to the node you want to connect to. e.g. https://hackathonnode.matchat.org/won");
-            failed = true;
-        }
-        if(System.getProperty("WON_CONFIG_DIR") == null && System.getenv("WON_CONFIG_DIR") == null) {
-            logger.error("WON_CONFIG_DIR needs to be set");
-            failed = true;
-        }
-
-        if (failed) {
+        if (!BotUtils.isValidRunConfig()) {
             System.exit(1);
         }
         SpringApplication app = new SpringApplication("classpath:/spring/app/botApp.xml");
